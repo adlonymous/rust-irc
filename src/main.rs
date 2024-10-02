af07a768728d9827b5b00b679e1187f806baf826
adlonymous <adlonymous@gmail.com>
2024-10-02 16:39:26 -0400

    Base TCP template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("RUCS IRC Client");
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:6667").await?;
+    println!("Server running on 127.0.0.1:6667");
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                if socket.write_all(&buf[0..n]).await.is_err() {
+                    eprintln!("failed to write to socket");
+                    return;
+                }
+            }
+        });
+    }
 }
